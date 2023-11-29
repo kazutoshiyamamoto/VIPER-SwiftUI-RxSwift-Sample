3aad3d946ed28f5828d44f39dee01e22654f15e5
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2023-11-29 14:26:11 +0900

    Viewで使用するDataSourceクラスを追加

--- VIPER-SwiftUI-RxSwift-Sample/VIPER-SwiftUI-RxSwift-Sample/SampleView.swift
+++ VIPER-SwiftUI-RxSwift-Sample/VIPER-SwiftUI-RxSwift-Sample/SampleView.swift
@@ -6,8 +6,17 @@
 //
 
 import SwiftUI
+import RxSwift
+
 
 struct SampleView: View {
+    class DataSource: ObservableObject, ReactiveCompatible {
+        @Published var isOrderProcessing: Bool = false
+        @Published var cartProducts: [String] = []
+    }
+    
+    @ObservedObject var dataSource: DataSource
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
