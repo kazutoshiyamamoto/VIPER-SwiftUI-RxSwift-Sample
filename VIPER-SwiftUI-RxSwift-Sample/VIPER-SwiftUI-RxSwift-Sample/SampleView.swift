b4620e27e74553d35801a2f9506e1d346118aaa7
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2023-11-29 14:19:48 +0900

    SwiftUIのViewを追加

---
+++ VIPER-SwiftUI-RxSwift-Sample/VIPER-SwiftUI-RxSwift-Sample/SampleView.swift
@@ -0,0 +1,18 @@
+//
+//  SampleView.swift
+//  VIPER-SwiftUI-RxSwift-Sample
+//
+//  Created by home on 2023/11/29.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SampleView()
+}
