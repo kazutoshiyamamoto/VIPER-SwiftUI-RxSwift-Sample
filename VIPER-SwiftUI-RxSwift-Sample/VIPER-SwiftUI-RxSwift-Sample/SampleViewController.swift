81b1a627e3918a320d48164ce6c98ba17d74afdc
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2023-12-02 00:05:32 +0900

    UIHostingControllerを使ってSwiftUIを使用する

--- VIPER-SwiftUI-RxSwift-Sample/VIPER-SwiftUI-RxSwift-Sample/SampleViewController.swift
+++ VIPER-SwiftUI-RxSwift-Sample/VIPER-SwiftUI-RxSwift-Sample/SampleViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import SwiftUI
 
 class SampleViewController: UIViewController {
 
+    private var dataSource: SampleView.DataSource = .init()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let rootView = SampleView(delegate: self, dataSource: dataSource)
+        let hostingVC = UIHostingController(rootView: rootView)
+        addChild(hostingVC)
+        hostingVC.didMove(toParent: self)
+        view.addSubview(hostingVC.view)
+        hostingVC.view.translatesAutoresizingMaskIntoConstraints = false
+        hostingVC.view.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        hostingVC.view.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
 
 
